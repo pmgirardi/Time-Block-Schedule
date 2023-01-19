1c12c71f5a1c620b91b7d76fede238ce83f38da5
pmgirardi <117805446+pmgirardi@users.noreply.github.com>
2023-01-18 23:27:19 -0800

    added current date to javascript file

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+
+// Display the current date and time using id of "currentDay" and day.js to format date
+var currentDate = dayjs().format('dddd, MMMM D'); 
+$('#currentDay').text(currentDate);
+
+
 $(function () {
     // TODO: Add a listener for click events on the save button. This code should
     // use the id in the containing time-block as a key to save the user input in
     // local storage.
+
   
   
     // HINT: What does `this` reference in the click listener

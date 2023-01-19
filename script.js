a52506501a93587f86f1f8c573bb64b554ee793b
pmgirardi <117805446+pmgirardi@users.noreply.github.com>
2023-01-19 12:06:23 -0800

    updated js file and debugged. Upated HTML file removing past, present, and future classes

--- script.js
+++ script.js
@@ -1,54 +1,52 @@
-
-$(function () {
-
 // Variables 
-var saveButtonEl = $('.saveBtn');
-
-var currentHour = $('dayjs().hour()');
+var currentHour =  dayjs().hour() 
+var timeBlockEl = document.querySelector('.container');
 
 // Display the current date and time using id of "currentDay" and day.js to format date
 var currentDate = dayjs().format('dddd, MMMM D'); 
 $('#currentDay').text(currentDate);
 
-// Event listener 
-saveButtonEl.on('click', function () {
-  var textValue = $(this).siblings('.description').val();
-  var timeKey = $(this).parent().attr('id');
 
-  localStorage.setItem(timeKey, textValue);
+// Event listener 
+$('.saveBtn').on('click', function () {
+
+  var value = $(this).siblings('.description').val();
+  var time = $(this).parent().attr('id');
+
+
+  localStorage.setItem(time, value);
 });
 
-$('#hour-9.description').val(localStorage.getItem('hour-9'));
-$('#hour-10.description').val(localStorage.getItem('hour-10'));
-$('#hour-11.description').val(localStorage.getItem('hour-11'));
-$('#hour-12.description').val(localStorage.getItem('hour-12'));
-$('#hour-13.description').val(localStorage.getItem('hour-13'));
-$('#hour-14.description').val(localStorage.getItem('hour-14'));
-$('#hour-15.description').val(localStorage.getItem('hour-15'));
-$('#hour-16.description').val(localStorage.getItem('hour-16'));
-$('#hour-17.description').val(localStorage.getItem('hour-17'));
+$('#hour8 .description').val(localStorage.getItem('hour8'));
+$('#hour9 .description').val(localStorage.getItem('hour9'));
+$('#hour10 .description').val(localStorage.getItem('hour10'));
+$('#hour11 .description').val(localStorage.getItem('hour11'));
+$('#hour12 .description').val(localStorage.getItem('hour12'));
+$('#hour13 .description').val(localStorage.getItem('hour13'));
+$('#hour14 .description').val(localStorage.getItem('hour14'));
+$('#hour15 .description').val(localStorage.getItem('hour15'));
+$('#hour16 .description').val(localStorage.getItem('hour16'));
+$('#hour17 .description').val(localStorage.getItem('hour17'));
 
-// jkjlkdf 
 
 function timeTracker() {
   $('.time-block').each(function () {
-    var timeId = parseInt($(this).attr('id').split("hour")[1]);
+    var timeId = $(this).attr("id").split("hour")[1];
+    console.log("currentHour " + currentHour)
+    console.log("timeId " + timeId)
 
-    
-
-
+    if (timeId < currentHour) {
+        $(this).addClass("past");
+    }
+    else if (timeId == currentHour) {
+        $(this).addClass("present");
+    }
+    else {
+        $(this).addClass("future");
+    }
+})
 }
-    //
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
-    //
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
-    //
-    // TODO: Add code to display the current date in the header of the page.
-  });
-  
+
+// Call timeTracker function 
+
+timeTracker();

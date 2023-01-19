ab3c9eaff3f2d4d44bf1fef9819b4fd1c74c1d1b
pmgirardi <117805446+pmgirardi@users.noreply.github.com>
2023-01-19 08:24:28 -0800

    added event listener, defined variables, and working on function to track tasks for past, present or future

--- script.js
+++ script.js
@@ -1,23 +1,43 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+
+$(function () {
+
+// Variables 
+var saveButtonEl = $('.saveBtn');
+
+var currentHour = $('dayjs().hour()');
 
 // Display the current date and time using id of "currentDay" and day.js to format date
 var currentDate = dayjs().format('dddd, MMMM D'); 
 $('#currentDay').text(currentDate);
 
+// Event listener 
+saveButtonEl.on('click', function () {
+  var textValue = $(this).siblings('.description').val();
+  var timeKey = $(this).parent().attr('id');
 
-$(function () {
-    // TODO: Add a listener for click events on the save button. This code should
-    // use the id in the containing time-block as a key to save the user input in
-    // local storage.
+  localStorage.setItem(timeKey, textValue);
+});
 
-  
-  
-    // HINT: What does `this` reference in the click listener
-    // function? How can DOM traversal be used to get the "hour-x" id of the
-    // time-block containing the button that was clicked? How might the id be
-    // useful when saving the description in local storage?
+$('#hour-9.description').val(localStorage.getItem('hour-9'));
+$('#hour-10.description').val(localStorage.getItem('hour-10'));
+$('#hour-11.description').val(localStorage.getItem('hour-11'));
+$('#hour-12.description').val(localStorage.getItem('hour-12'));
+$('#hour-13.description').val(localStorage.getItem('hour-13'));
+$('#hour-14.description').val(localStorage.getItem('hour-14'));
+$('#hour-15.description').val(localStorage.getItem('hour-15'));
+$('#hour-16.description').val(localStorage.getItem('hour-16'));
+$('#hour-17.description').val(localStorage.getItem('hour-17'));
+
+// jkjlkdf 
+
+function timeTracker() {
+  $('.time-block').each(function () {
+    var timeId = parseInt($(this).attr('id').split("hour")[1]);
+
+    
+
+
+}
     //
     // TODO: Add code to apply the past, present, or future class to each time
     // block by comparing the id to the current hour. HINTS: How can the id
